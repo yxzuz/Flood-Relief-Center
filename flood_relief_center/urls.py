7d44436c01c5a1275dd6dbac7e9f8e296aa03502
Natcha Ranchan <natcha.ran@ku.th>
2024-11-13 19:51:50 +0700

    urls and view

--- flood_relief_center/urls.py
+++ flood_relief_center/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-app_name= "flood_relief_center"
+app_name= "flood-relief-center"
 urlpatterns = [
     path("", views.index, name="index"),
     path("relief-centers/", views.ReliefCentersListView.as_view(), name="relief-centers"),
+    path("donations/", views.DonationListView.as_view(), name="donations"),
+    path("relief-centers/<int:centerID>/", views.ReliefCenterDetailView.as_view(), name="relief-center-detail"),
+    path("victims/", views.VictimsListView.as_view(), name="victims"),
 ]
--- flood_relief_center/views.py
+++ flood_relief_center/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import ReliefCenter
+from .models import ReliefCenter, Donation, Victim
 
 # Create your views here.
 
@@ -9,10 +9,28 @@ def index(request):
     return render(request, "flood_relief_center/index.html")
 
 
-# def show_relief_centers(request):
-#     return render(request, "flood_relief_center/relief_centers.html")
-
 class ReliefCentersListView(ListView):
     model = ReliefCenter
     context_object_name = "relief_centers_lists"
     template_name = "flood_relief_center/relief_centers.html"
+    
+    
+class DonationListView(ListView):
+    model = Donation
+    context_object_name = "donation_lists"
+    template_name = "flood_relief_center/donations.html"
+    
+
+class VictimsListView(ListView):
+    model = Victim
+    context_object_name = "victim_lists"
+    template_name = "flood_relief_center/victims.html"
+    
+    
+class ReliefCenterDetailView(ListView):
+    model = ReliefCenter
+    context_object_name = "relief_center_detail"
+    template_name = "flood_relief_center/relief_center_detail.html"
+    
+    def get_queryset(self):
+        return ReliefCenter.objects.filter(centerID=self.kwargs.get('centerID'))  # Filter by centerID
