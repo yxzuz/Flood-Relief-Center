d3a9908278eb023b418a1e9f3e3ddfc54ed2e599
Natcha Ranchan <natcha.ran@ku.th>
2024-11-15 00:08:19 +0700

    user can filter victim query

--- flood_relief_center/views.py
+++ flood_relief_center/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import ReliefCenter, Donation, Victim
+from django.db.models import Q
+from .models import ReliefCenter, Donation, Victim, AffectedArea
 
 # Create your views here.
+CENTER = [center.name for center in ReliefCenter.objects.all()]
+STATUS = [('safe', 'Safe'), ('injured', 'Injured'), ('missing', 'Missing')]
+RISK_LEVEL = [(1, 'Low'), (2, 'Moderate'), (3, 'High'),
+              (4, 'Critical'), (5, 'Severe')]
 
 
 def index(request):
@@ -13,24 +18,77 @@ class ReliefCentersListView(ListView):
     model = ReliefCenter
     context_object_name = "relief_centers_lists"
     template_name = "flood_relief_center/relief_centers.html"
-    
-    
+
+
 class DonationListView(ListView):
     model = Donation
     context_object_name = "donation_lists"
     template_name = "flood_relief_center/donations.html"
-    
+
 
 class VictimsListView(ListView):
     model = Victim
     context_object_name = "victim_lists"
     template_name = "flood_relief_center/victims.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["centers"] = CENTER
+        context["current_status"] = STATUS
+        context["risk_level"] = RISK_LEVEL
+        return context
+
     
-    
+    def get_search_query(self, queryset, search_query):
+        if search_query:
+            queryset = queryset.filter(Q(name__icontains=search_query) | Q(victimNumber__icontains=search_query) | Q(address__icontains=search_query) | Q(center__name__icontains=search_query) | Q(riskLevel__icontains=search_query) | Q(currentStatus__icontains=search_query))
+        return queryset
+        
+    def get_queryset(self):
+        queryset = Victim.objects.all()
+
+        # Get the query parameters from the request
+        search_query = self.request.GET.get("search_query", "")
+        selected_center = self.request.GET.get("selected_center", "")
+        selected_status = self.request.GET.get("selected_status", "")
+        selected_risk_level = self.request.GET.get("selected_risk_level", "")
+        # age_range = self.request.GET.get("age_range", None)
+
+        # Apply search filter
+        if search_query:
+            queryset = self.get_search_query(queryset, search_query)
+
+
+        # Apply center filter
+        if selected_center:
+            queryset = queryset.filter(center__name=selected_center)
+
+        # Apply status filter
+        if selected_status:
+            queryset = queryset.filter(currentStatus=selected_status)
+
+        # Apply risk level filter
+        if selected_risk_level:
+            queryset = queryset.filter(riskLevel=selected_risk_level)
+
+        # Apply age range filter (if provided)
+        # if age_range:
+        #     queryset = queryset.filter(age__lte=age_range)
+
+        return queryset
+
+
+class AffectedAreaListView(ListView):
+    model = AffectedArea
+    context_object_name = "affected_area_lists"
+    template_name = "flood_relief_center/affected_areas.html"
+
+
 class ReliefCenterDetailView(ListView):
     model = ReliefCenter
     context_object_name = "relief_center_detail"
     template_name = "flood_relief_center/relief_center_detail.html"
-    
+
     def get_queryset(self):
-        return ReliefCenter.objects.filter(centerID=self.kwargs.get('centerID'))  # Filter by centerID
+        # Filter by centerID
+        return ReliefCenter.objects.filter(centerID=self.kwargs.get('centerID'))
